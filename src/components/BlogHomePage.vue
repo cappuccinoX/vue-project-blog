c869f79d95249e56ccde6fc1cda99564efd00449
452776965@qq.com <452776965@qq.com>
2019-05-22 17:19:40 +0800

    fst commit

---
+++ src/components/BlogHomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="blog_homepage">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li><a target="_blank" href="https://nodejs.org/">Node.js</a></li>
+      <li><a target="_blank" href="https://www.javascript.com/">JavaScript</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'blog homepage',
+  data () {
+    return {
+      msg: 'Welcome to my blog!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  font-size: 50px
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: darkblue;
+  font-size: 30px;
+}
+</style>
---
+++ src/components/tab.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="nav-list">
+    <!--<li v-for = "(item) in tabs" v-bind:key="item.name">-->
+       <!--{{item.name}}-->
+    <!--</li>-->
+    <li><a>node</a></li>
+    <li><a>js</a></li>
+  </ul>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      tabs: [{name: 'Node.js', link: 'https://nodejs.org/'}, {name: 'Javascript', link: 'https://www.javascript.com/'}]
+    }
+  }
+}
+</script>
+
+<!--<div id="app">-->
+  <!--<ol>-->
+    <!--<li v-for="site in sites">-->
+      <!--{{ site.name }}-->
+    <!--</li>-->
+  <!--</ol>-->
+<!--</div>-->
+
+<!--<script>-->
+<!--export default{-->
+  <!--data () {-->
+    <!--return {-->
+      <!--sites: [-->
+        <!--{ name: 'Runoob' },-->
+        <!--{ name: 'Google' },-->
+        <!--{ name: 'Taobao' }-->
+      <!--]-->
+    <!--}-->
+  <!--}-->
+<!--}-->
+<!--</script>-->
+<style>
+  li {
+    color: black;
+  }
+</style>
